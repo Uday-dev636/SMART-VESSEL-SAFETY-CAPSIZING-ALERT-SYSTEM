aacaf13c12ce64fd5b679e8cf23ab7e2b4909396
Uday-dev636 <naliudaykumar63@gmail.com>
2026-04-08 06:22:34 +0530

    Update whthersation.c

--- code/whthersation.c
+++ code/whthersation.c
@@ -5,86 +5,82 @@
 #include <Adafruit_BMP280.h>
 #include <DHT.h>
 
-// ---------------- TFT PINS ----------------
-#define TFT_CS   5
-#define TFT_DC   2
-#define TFT_RST  4
+// -------- TFT --------
+#define TFT_CS   D2
+#define TFT_DC   D3
+#define TFT_RST  D4
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
-// ---------------- DHT22 ----------------
-#define DHTPIN 15
+// -------- DHT22 --------
+#define DHTPIN D6
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-// ---------------- BMP280 ----------------
+// -------- BMP280 --------
 Adafruit_BMP280 bmp;
 
-// ---------------- SETUP ----------------
+// -------- SETUP --------
 void setup() {
   Serial.begin(115200);
 
+  // I2C
+  Wire.begin(D2, D1); // SDA, SCL
+
+  // TFT
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
 
+  // Sensors
   dht.begin();
 
   if (!bmp.begin(0x76)) {
-    Serial.println("BMP280 not found!");
+    tft.println("BMP280 ERROR!");
     while (1);
   }
 
-  tft.setTextColor(ILI9341_WHITE);
   tft.setTextSize(2);
+  tft.setTextColor(ILI9341_WHITE);
 }
 
-// ---------------- LOOP ----------------
+// -------- LOOP --------
 void loop() {
 
   float temp = dht.readTemperature();
   float hum  = dht.readHumidity();
-  float pressure = bmp.readPressure() / 100.0; // hPa
+  float pressure = bmp.readPressure() / 100.0;
 
-  // Clear screen
   tft.fillScreen(ILI9341_BLACK);
 
-  // Title
   tft.setCursor(10, 10);
-  tft.setTextSize(2);
   tft.println("Weather Station");
 
-  // Temperature
   tft.setCursor(10, 50);
   tft.print("Temp: ");
   tft.print(temp);
   tft.println(" C");
 
-  // Humidity
   tft.setCursor(10, 90);
   tft.print("Humidity: ");
   tft.print(hum);
   tft.println(" %");
 
-  // Pressure
   tft.setCursor(10, 130);
   tft.print("Pressure: ");
   tft.print(pressure);
   tft.println(" hPa");
 
-  // -------- CYCLONE PREDICTION --------
+  // Cyclone logic
   tft.setCursor(10, 180);
-  tft.setTextSize(2);
 
   if (hum < 60 && pressure < 1000) {
     tft.setTextColor(ILI9341_RED);
     tft.println("Cyclone Risk!");
-  }
-  else if (hum < 60) {
+  } else if (hum < 60) {
     tft.setTextColor(ILI9341_YELLOW);
     tft.println("Humidity Drop!");
-  }
-  else {
+  } else {
     tft.setTextColor(ILI9341_GREEN);
     tft.println("Weather Normal");
   }
